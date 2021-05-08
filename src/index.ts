c5c3ba6efcc0927a487036b8ee3ee6e2c2270971
lijinrong <306372058@qq.com>
2021-05-08 16:07:48 +0800

    feat: 修复异步读写文件问题

--- src/index.ts
+++ src/index.ts
@@ -17,16 +17,10 @@ const imgUploadLoader = async function (source: Buffer): Promise<void> {
   const callback = this.async();
   // @ts-ignore
   const options = getOptions(this);
-  try {
-    // @ts-ignore
-    const data = await Process(source, options, this);
-    callback(null, `module.exports = '${data.url}'`);
-    return;
-  } catch (e) {
-    // @ts-ignore
-    this.emitError(e);
-    throw e;
-  }
+  // @ts-ignore
+  const data = await Process(source, options, this);
+  // @ts-ignore
+  callback(null, `module.exports = '${data.url}'`);
 };
 
 // 上传
@@ -36,6 +30,20 @@ function upload(url: string, file: Buffer): Promise<Data> {
       method: 'POST',
       url: url,
       data: file,
+      headers: {
+        Accept:
+          'image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8',
+        'Content-Type': 'application/octet-stream',
+        Connection: 'keep-alive',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language':
+          'zh-CN,zh;q=0.9,en;q=0.8,es-MX;q=0.7,es;q=0.6,tr;q=0.5,id;q=0.4,hi;q=0.3,en-US;q=0.2,th;q=0.1,ru;q=0.1,uz;q=0.1',
+        'Sec-Fetch-Dest': 'image',
+        'Sec-Fetch-Mode': 'no-cors',
+        'Sec-Fetch-Site': 'cross-site',
+        'User-Agent':
+          'Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1',
+      },
     })
       .then((response) => {
         res(response.data);
@@ -70,7 +78,10 @@ function genMd5(file: Buffer): string {
 function checkUrlMap(hash: string, cachePath: string, context: any): any {
   // 项目特殊性，需要读取dist里面的配置
   const filePath = cachePath;
-  const key = context.resourcePath.replace(context.options.context, '');
+  const key = context.resourcePath.replace(
+    context.rootContext || context.options.context,
+    '',
+  );
   try {
     // 已有缓存的json文件
     const file = fs.readFileSync(path.join(filePath, cacheFileName), 'utf-8');
@@ -89,14 +100,21 @@ function checkUrlMap(hash: string, cachePath: string, context: any): any {
 // 写入url-map，为下次编译缓存
 function genUrlMap(hash: string, url: string, cachePath: string, context: any) {
   const filePath = cachePath;
-  const key: string = context.resourcePath.replace(context.options.context, '');
+  const key: string = context.resourcePath.replace(
+    context.rootContext || context.options.context,
+    '',
+  );
   let result: any;
   try {
     const file = fs.readFileSync(path.join(filePath, cacheFileName), 'utf-8');
     result = JSON.parse(file);
     // 检查脏数据并删除
     Object.keys(result).forEach((item) => {
-      if (!fs.existsSync(`${context.options.context}${item}`)) {
+      if (
+        !fs.existsSync(
+          `${context.rootContext || context.options.context}${item}`,
+        )
+      ) {
         delete result[item];
       }
     });
@@ -119,16 +137,14 @@ function genUrlMap(hash: string, url: string, cachePath: string, context: any) {
     }
   }
   const Str_ans = JSON.stringify(result, null, 4);
-  fs.writeFile(path.join(filePath, cacheFileName), Str_ans, 'utf8', (err) => {
-    if (err) throw err;
-  });
+  fs.writeFileSync(path.join(filePath, cacheFileName), Str_ans, 'utf8');
 }
 
 async function Process(
   file: Buffer,
   options: any,
   context: any,
-): Promise<Data> {
+): Promise<Data | void> {
   const md5 = genMd5(file);
   const cachePath = options.cachePath
     ? options.cachePath
@@ -136,13 +152,24 @@ async function Process(
   const url = checkUrlMap(md5, cachePath, context);
   const isUpload = !!url;
   if (!isUpload) {
-    const key = context.resourcePath.replace(context.options.context, '');
-		const spinner = ora(`uploading images: ${key}`);
+    const key = context.resourcePath.replace(
+      context.rootContext || context.rootContext || context.options.context,
+      '',
+    );
+    const spinner = ora(`uploading images: ${key}`);
     spinner.start();
-    const data = await upload(options.url, file);
-    genUrlMap(md5, data.url, cachePath, context);
-    spinner.succeed();
-    return data;
+    try {
+      const data = await upload(options.url, file);
+      genUrlMap(md5, data.url, cachePath, context);
+      spinner.succeed();
+      return data;
+    } catch (e) {
+      console.log(e.toJSON());
+      if (e.code === 'ECONNRESET') {
+        console.log('重试中', file);
+        return await Process(file, options, context);
+      }
+    }
   } else {
     return {
       url,
